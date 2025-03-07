ef0c9134ec2bbbd230483bb434367d5b068fbd4a
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-03-07 14:47:08 -0300

    feat: it initializes the Nimble library

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <Arduino.h>
+#include "NimBLEDevice.h"
+#include "NimBLEAddress.h"
 
 void setup()
 {
   Serial.begin(115200);
+  Serial.printf("Iniciando setup...\n");
+
+  NimBLEDevice::init("");
+
+  Serial.printf("Setup finalizado!\n");
 }
 
 void loop()
 {
+  Serial.printf("Loop\n");
+  delay(5000);
 }
