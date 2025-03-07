f2c751c87a6f9d0888b7650b32b2bad2e9e5b4c1
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-03-07 17:33:33 -0300

    feat: it adds the first led configs

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include "NimBLEDevice.h"
 
+#define RED_LED 13
+#define GREEN_LED 15
+
 #define SCANNING_TIME 5
 #define MAC_ADDRESS "ff:ff:11:1f:7d:a2"
 
@@ -10,6 +13,8 @@ void setup()
 {
   Serial.begin(115200);
   NimBLEDevice::init("");
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
 }
 
 void loop()
