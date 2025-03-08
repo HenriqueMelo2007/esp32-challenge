7036b01831d44701d24e759b3a7003721cd3b13e
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-03-08 12:00:18 -0300

    style: it adds comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,30 @@
-#include <Arduino.h>
+#include <Arduino.h> // Inclusão das Bibliotecas e Definição dos Macros
 #include "NimBLEDevice.h"
 
 #define RED_LED 13
 #define GREEN_LED 15
 
 #define SCANNING_TIME 2
-#define MAC_ADDRESS_ONE "ff:ff:11:1f:7d:a2"
+#define MAC_ADDRESS_ONE "ff:ff:11:1f:7d:a2" 
 #define MAC_ADDRESS_TWO "ff:ff:11:1f:26:53"
 
-volatile bool tagFound = false;
+volatile bool tagFound = false; // Declaração da Variável Global tagFound
 
-bool scanning();
+bool scanning(); // Protótipos das Funções Utilizadas
 void scanningTask(void *parameter);
 void blinkRedLED(int onTime, int offTime);
 
-void setup()
+void setup() // Configuração Inicial no setup()
 {
   Serial.begin(115200);
-  NimBLEDevice::init("");
+  NimBLEDevice::init(""); 
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
 
-  xTaskCreate(scanningTask, "ScanningTask", 4096, NULL, 1, NULL);
+  xTaskCreate(scanningTask, "ScanningTask", 4096, NULL, 1, NULL); // Criação da tarefa do FreeRTOS
 }
 
-void loop()
+void loop() // Loop Principal do Programa
 {
   if (tagFound)
   {
@@ -38,18 +38,18 @@ void loop()
   }
 }
 
-bool scanning()
+bool scanning() // Função scanning() – Escaneamento de Dispositivos BLE
 {
   NimBLEScan *scan = NimBLEDevice::getScan();
-  NimBLEScanResults scanResult = scan->getResults(SCANNING_TIME * 1000);
+  NimBLEScanResults scanResult = scan->getResults(SCANNING_TIME * 1000); // Obtém o conjunto de resultados do scan
 
-  for (int i = 0; i < scanResult.getCount(); i++)
+  for (int i = 0; i < scanResult.getCount(); i++) // Itera sobre cada item do conjunto de resultados
   {
-    const NimBLEAdvertisedDevice *specificDevice = scanResult.getDevice(i);
-    NimBLEAddress specificMacAddress = specificDevice->getAddress();
-    const char *addressString = specificMacAddress.toString().c_str();
+    const NimBLEAdvertisedDevice *specificDevice = scanResult.getDevice(i); // Obtém item específico na ordem do loop
+    NimBLEAddress specificMacAddress = specificDevice->getAddress(); // Obtém endereço MAC para comparação posterior
+    const char *addressString = specificMacAddress.toString().c_str(); // Endereço em string para comparação
 
-    if (strcmp(addressString, MAC_ADDRESS_ONE) == 0 || strcmp(addressString, MAC_ADDRESS_TWO) == 0)
+    if (strcmp(addressString, MAC_ADDRESS_ONE) == 0 || strcmp(addressString, MAC_ADDRESS_TWO) == 0) // Uso de strcmp() para comparar MAC do iterador com macro do MAC das tags
     {
       Serial.println("*** TAG encontrada ***");
       return true;
@@ -59,22 +59,22 @@ bool scanning()
   return false;
 }
 
-void scanningTask(void *parameter)
+void scanningTask(void *parameter) // Função scanningTask() – Tarefa do FreeRTOS para Escaneamento Contínuo
 {
-  while (true)
+  while (true) // Scan infinito
   {
     tagFound = scanning();
     vTaskDelay(10 / portTICK_PERIOD_MS);
   }
 }
 
-inline void blinkRedLED(int onTime, int offTime)
+inline void blinkRedLED(int onTime, int offTime) // Função blinkRedLED() – Controle Não Bloqueante do LED Vermelho
 {
-  static unsigned long previousMillis = 0;
+  static unsigned long previousMillis = 0; // Variáveis usadas para calcular se o LED deve ou não mudar de estado ( on - off )
   static int ledState = LOW;
   unsigned long currentMillis = millis();
 
-  if (previousMillis == 0)
+  if (previousMillis == 0) // Primeiro acionamento
   {
     digitalWrite(RED_LED, HIGH);
     ledState = HIGH;
@@ -82,9 +82,9 @@ inline void blinkRedLED(int onTime, int offTime)
     return;
   }
 
-  unsigned long elapsed = currentMillis - previousMillis;
+  unsigned long elapsed = currentMillis - previousMillis; // Variável que armazena a diferença de tempo entre o momento atual e a última mudança de estado do LED
 
-  if (ledState == HIGH && elapsed >= onTime)
+  if (ledState == HIGH && elapsed >= onTime) // Estrutura condicional para definir o estado do LED vermelho
   {
     digitalWrite(RED_LED, LOW);
     ledState = LOW;
